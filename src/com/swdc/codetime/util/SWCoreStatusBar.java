1493473b7f5b75b67821f7e2022389bf24d3149e
Xavier <xavier@yottamo.com>
2020-05-04 10:10:10 -0700

    updated status bar item layout render logic

--- src/com/swdc/codetime/util/SWCoreStatusBar.java
+++ src/com/swdc/codetime/util/SWCoreStatusBar.java
@@ -2,18 +2,14 @@ package com.swdc.codetime.util;
 
 import org.eclipse.jface.action.LegacyActionTools;
 import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.resource.JFaceColors;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CLabel;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
-import org.eclipse.ui.ISharedImages;
-import org.eclipse.ui.PlatformUI;
 
 import com.swdc.codetime.CodeTimeActivator;
 
@@ -36,7 +32,8 @@ public class SWCoreStatusBar extends org.eclipse.ui.menus.WorkbenchWindowControl
 
 	private String tooltip = "";
 
-	private String text = "                                 ";
+	// initialize with a long string
+	private String text = "Active Code Time";
 
 	private String iconName;
 
@@ -103,10 +100,11 @@ public class SWCoreStatusBar extends org.eclipse.ui.menus.WorkbenchWindowControl
 		layout.marginBottom = 0;
 		layout.marginHeight = 0;
 		layout.marginWidth = 0;
+		layout.wrap = false;
 		composite.setLayout(layout);
 		new Label(composite, SWT.SEPARATOR);
 
-		label = new CLabel(composite, SWT.SHADOW_NONE);
+		label = new CLabel(composite, SWT.BOLD);
 
 		update();
 
@@ -117,41 +115,42 @@ public class SWCoreStatusBar extends org.eclipse.ui.menus.WorkbenchWindowControl
 
 	public void update() {
 		if (label != null && !label.isDisposed()) {
-			Display display = label.getDisplay();
-			if (errorText != null && errorText.length() > 0) {
-				label.setForeground(JFaceColors.getErrorText(display));
-				label.setText(escape(errorText));
-				label.setImage(PlatformUI.getWorkbench().getSharedImages().getImage(ISharedImages.IMG_OBJS_ERROR_TSK));
-				if (errorDetail != null)
+			label.setForeground(label.getParent().getForeground());
+			if (errorText != null && !errorText.equals("")) {
+				if (errorDetail != null) {
 					label.setToolTipText(escape(errorDetail));
-				else if (tooltip != null)
+				} else if (tooltip != null) {
 					label.setToolTipText(escape(tooltip));
-				else
-					label.setToolTipText(null);
-
-			} else {
-				label.setForeground(label.getParent().getForeground());
-				label.setText(escape(text));
-				if (iconName != null) {
-					ImageDescriptor imgDescriptor = SWCoreImages.create("icons/", iconName);
-					try {
-						// the parent layout update should handle image updates, no need to dispose
-//						if (label.getImage() != null) {
-//							label.getImage().dispose();
-//						}
-						label.setImage(SWCoreImages.getImage(imgDescriptor));
-					} catch (Exception e) {
-						SWCoreLog.log(e);
-					}
 				}
-
-				if (tooltip != null)
-					label.setToolTipText(escape(tooltip));
-				else
-					label.setToolTipText(null);
+				label.setText("Code Time");
+			} else {
+				if ((text == null || text.trim().equals("")) && !iconName.equals("clock.png")) {
+					text = "Code Time";
+				} else {
+					text = text.trim();
+				}
 			}
+			iconName = iconName == null || iconName.trim().equals("") ? iconName = "paw.png" : iconName;
+
+			try {
+				// the parent layout update should handle image updates, no need to dispose
+				// if (label.getImage() != null) {
+				// label.getImage().dispose();
+				// }
+				ImageDescriptor imgDescriptor = SWCoreImages.create("icons/", iconName);
+				label.setImage(SWCoreImages.getImage(imgDescriptor));
+			} catch (Exception e) {
+				SWCoreLog.log(e);
+			}
+
+			if (tooltip != null) {
+				label.setToolTipText(tooltip.trim());
+			}
+			label.setText(text);
 		}
+
 		try {
+			label.layout(true);
 			label.getParent().layout(true);
 		} catch (Exception e) {
 			SWCoreLog.error("Unable to render the status bar text", e);
