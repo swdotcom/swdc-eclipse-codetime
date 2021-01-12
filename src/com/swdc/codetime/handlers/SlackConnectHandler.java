66fe2298746ee879ff42c5c05f45f884855f8ad1
Xavier Luiz <xavluiz@gmail.com>
2021-01-11 17:34:28 -0800

    implement slack status update logic and prep for deployment

--- src/com/swdc/codetime/handlers/SlackConnectHandler.java
+++ src/com/swdc/codetime/handlers/SlackConnectHandler.java
@@ -1,5 +1,7 @@
 package com.swdc.codetime.handlers;
 
+import javax.swing.SwingUtilities;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -12,8 +14,10 @@ public class SlackConnectHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		SlackManager.connectSlackWorkspace(() -> {
-			WallClockManager.refreshTree();
+		SwingUtilities.invokeLater(() -> {
+			SlackManager.connectSlackWorkspace(() -> {
+				WallClockManager.refreshTree();
+			});
 		});
 		return null;
 	}
--- src/com/swdc/codetime/handlers/SlackDisconnectHandler.java
+++ src/com/swdc/codetime/handlers/SlackDisconnectHandler.java
@@ -1,5 +1,7 @@
 package com.swdc.codetime.handlers;
 
+import javax.swing.SwingUtilities;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -8,16 +10,18 @@ import com.swdc.codetime.managers.WallClockManager;
 
 import swdc.java.ops.manager.SlackManager;
 
-public class SlackDisconnectHandler extends AbstractHandler{
+public class SlackDisconnectHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		SlackManager.disconnectSlackWorkspace(() -> {
-			WallClockManager.refreshTree();
+		SwingUtilities.invokeLater(() -> {
+			SlackManager.disconnectSlackWorkspace(() -> {
+				WallClockManager.refreshTree();
+			});
 		});
 		return null;
 	}
-	
+
 	@Override
 	public boolean isEnabled() {
 		return SlackManager.hasSlackWorkspaces();
